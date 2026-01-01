c190230dd8d4bd15a5718cdc3a45bd3e1243be59
InGraw <166131364+InGraw-Co@users.noreply.github.com>
2026-01-01 17:32:04 +0100

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,5 +1,4 @@
 @echo off
-REM Change to script directory
 cd /d %~dp0
 
 IF EXIST ".venv\Scripts\activate" (
@@ -16,10 +15,11 @@ IF EXIST ".venv\Scripts\activate" (
   python -m pip install -r requirements.txt || py -3 -m pip install -r requirements.txt
 )
 
-REM Check tkinter availability
 python -c "import tkinter" 2>nul || (
   echo.
   echo WARNING: tkinter is not available in this environment.
   echo - Windows: make sure you installed Python from python.org with Tcl/Tk support.
 )
 echo All done.
+
+pause
